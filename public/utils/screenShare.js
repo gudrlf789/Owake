9aa329befd7ba706084e441a60a749c76f41cf01
park hyeong gil <gudrlf7913@naver.com>
2021-11-14 21:02:56 +0900

    불필요하게 소켓 접속하는 코드 삭제

--- public/utils/screenShare.js
+++ public/utils/screenShare.js
@@ -1,13 +1,11 @@
-const screenShareBtn = document.querySelector("#shareScreen");
+const screenShareBtn = document.getElementById("shareScreen");
 
 screenShareBtn.addEventListener("click", screenShareJoin);
 
 async function screenShareJoin() {
     const screenClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-
     await screenClient.join(options.appid, options.channel, null);
-    socket.emit("join_room", options.channel);
-
+    
     const screenTrack = await AgoraRTC.createScreenVideoTrack();
     await screenClient.publish(screenTrack);
     screenTrack.on("track-ended", () => {
