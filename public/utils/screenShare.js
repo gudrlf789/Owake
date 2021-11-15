3e503db9943df5f1990799876e6f06733cf41975
park hyeong gil <gudrlf7913@naver.com>
2021-11-15 10:29:06 +0900

    불필요한 socket.emit 코드 삭제

--- public/utils/screenShare.js
+++ public/utils/screenShare.js
@@ -5,7 +5,6 @@ screenShareBtn.addEventListener("click", screenShareJoin);
 async function screenShareJoin() {
     const screenClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
     await screenClient.join(options.appid, options.channel, null);
-    socket.emit("join-room", options.channel);
 
     const screenTrack = await AgoraRTC.createScreenVideoTrack();
     await screenClient.publish(screenTrack);
