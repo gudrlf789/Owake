cdb0ec85454d0bd7627570f4414d6e6a618bacb8
DeclanJeon <syas0301@gmail.com>
2021-12-04 22:31:07 +0900

    full request merge / server에 app.get(/room ) 지워진거 추가

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const bodyParder = require('body-parser');
+const bodyParder = require("body-parser");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const firebase = require("firebase/app");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const firebase = require('firebase/app');
-require('firebase/firestore');
-const firebaseConfig = require('./config/firebaseConfig.js');
+require("firebase/firestore");
+const firebaseConfig = require("./config/firebaseConfig.js");
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -36,73 +36,86 @@ app.use(express.static(path.join(__dirname, "../public/lib")));
 app.use(express.static(path.join(__dirname, "../public/utils")));
 app.use(express.static(path.join(__dirname, "../public/img/favicon")));
 app.use(express.static(path.join(__dirname, "../views")));
-app.use(bodyParder({
-    extended: true
-}));
+app.use(
+    bodyParder({
+        extended: true,
+    })
+);
 app.use(bodyParder.json());
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.post('/room/register', async (req, res) => {
+app.get("/room", (req, res, next) => {
+    res.render("room");
+});
+
+app.post("/room/register", async (req, res) => {
     const bodyData = req.body;
-    const snapshot = await db.collection("RoomList").where("roomName", "==", bodyData.roomName).get();
-    
-    if(snapshot.empty){
+    const snapshot = await db
+        .collection("RoomList")
+        .where("roomName", "==", bodyData.roomName)
+        .get();
+
+    if (snapshot.empty) {
         // doc에 특정 이름을 설정하고 싶을때
         //db.collection("RoomList").doc(bodyData.roomName).set(bodyData)
-        db.collection("RoomList").add({
-            adminId: bodyData.adminId,
-            adminPassword: bodyData.adminPassword,
-            roomName: bodyData.roomName,
-            roomType: bodyData.roomType,
-            roomPassword: bodyData.roomPassword,
-            roomTheme: bodyData.roomTheme,
-            roomIntroduce: bodyData.roomIntroduce
-        })
-        .then((e) => {
-            return res.status(200).json({
-                success: true
+        db.collection("RoomList")
+            .add({
+                adminId: bodyData.adminId,
+                adminPassword: bodyData.adminPassword,
+                roomName: bodyData.roomName,
+                roomType: bodyData.roomType,
+                roomPassword: bodyData.roomPassword,
+                roomTheme: bodyData.roomTheme,
+                roomIntroduce: bodyData.roomIntroduce,
             })
-        })
-        .catch((err) => {
-            return res.status(500).json({
-                success: false,
-                error: err
+            .then((e) => {
+                return res.status(200).json({
+                    success: true,
+                });
+            })
+            .catch((err) => {
+                return res.status(500).json({
+                    success: false,
+                    error: err,
+                });
             });
-        })
-    }else{
+    } else {
         return res.status(200).json({
-            success : false
+            success: false,
         });
     }
 });
 
-app.post('/room/search', async (req, res) => {
+app.post("/room/search", async (req, res) => {
     const bodyData = req.body;
     const roomArray = [];
 
-    db.collection("RoomList").where('roomName', '>=', bodyData.roomName).where('roomName', '<=', bodyData.roomName + '\uf8ff').get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-           roomArray.push(doc.data());
+    db.collection("RoomList")
+        .where("roomName", ">=", bodyData.roomName)
+        .where("roomName", "<=", bodyData.roomName + "\uf8ff")
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                roomArray.push(doc.data());
+            });
+
+            return res.status(200).json({
+                success: true,
+                roomList: roomArray,
+            });
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                success: false,
+                error: err,
+            });
         });
-
-        return res.status(200).json({
-            success: true,
-            roomList: roomArray
-        })
-    }).catch((err) => {
-        return res.status(500).json({
-            success: false,
-            error: err
-        })
-    });
 });
 
-app.post('/room/update', async (req, res) => {
-
-});
+app.post("/room/update", async (req, res) => {});
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomName) => {
