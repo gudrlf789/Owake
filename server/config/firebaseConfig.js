9bc86aa9b0e1beb501e8e174c98c4f4465d0ea4f
park hyeong gil <gudrlf7913@naver.com>
2021-12-06 10:58:47 +0900

    파이어베이스 설정 다시 스트링 타입으로 작성

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -2,11 +2,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 module.exports = firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_I,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBpd6nq3grmLc2waySsibG2HNJQEZByRlU",
+    authDomain: "owakeme-e9aa7.firebaseapp.com",
+    projectId: "owakeme-e9aa7",
+    storageBucket: "owakeme-e9aa7.appspot.com",
+    messagingSenderId: "772053587876",
+    appId: "1:772053587876:web:721ad4ece17ad853341331",
+    measurementId: "G-NC7JYR8DT0"
 };
