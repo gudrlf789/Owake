8d46a8a29dc5b2ccb01a47384ad5361b5e018807
park hyeong gil <gudrlf7913@naver.com>
2021-12-06 10:59:20 +0900

    채널 데이터 가져오는 코드 작성

--- public/utils/channelCreate.js
+++ public/utils/channelCreate.js
@@ -1,21 +1,19 @@
 $("#create").click((e) => {
-  //location.href = "/test";
   const reqData = {
        adminId: $('#adminId').val(),
        adminPassword: $('#adminPassword').val(),
-       roomName: "비비빅",
-       roomType: "private",
-       roomPassword: "123456",
+       roomType: $(':input:radio[name=drone]:checked').val(),
+       roomName: $('#channelName').val(),
+       roomPassword: $('#channelPassword').val(),
        roomTheme: $('#theme-category').val(),
-       roomIntroduce: $('#channel-description').val()
+       roomDescription: $('#channel-description').val()
    }
-   
    axios.post('/room/register', reqData)
    .then(res => {
        if (res.data.success) {
            alert("To room setting is success")
        }else{
-           alert(`RoomNumber is already existed. please choice another number`)
+           alert(`ChannelName: ${$('#channelName').val()} is already existed. please choice another number`)
        }
    });
 
