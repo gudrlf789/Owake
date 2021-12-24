774b85bd8440d66b672a6c78b9f351e9092259a7
park hyeong gil <gudrlf7913@naver.com>
2021-12-24 17:38:18 +0900

    public, private 방 join하는 버튼 따로 구분, 크로노사 방 들어갈수 있게 수정

--- public/utils/parts/channelJoin.js
+++ public/utils/parts/channelJoin.js
@@ -4,5 +4,41 @@ $("#channelJoin-btn").click((e) => {
 
     window.sessionStorage.setItem("channel", channelName);
     window.sessionStorage.setItem("uid", userId);
+
+    //테스트
+    window.location.href="http://localhost:1227/join";
+});
+
+$("#private-channelJoin-btn").click((e) => {
+    const channelName = $("#private-channelName").val();
+    const checkingPassword = $("#private-passwordChecking").val();
+
+    const userId = $("#private-nickName").val();
+    const password = $("#private-roomPassword").val();
+
+    if(checkingPassword !== password){
+        alert("Wrong Password");
+        $("#private-channelName").val("");
+        $("#private-passwordChecking").val("");
+        $("#private-nickName").val("");
+        $("#private-roomPassword").val("");
+        $("#channelPrivateJoin").modal("hide");
+    }else{
+        window.sessionStorage.setItem("channel", channelName);
+        window.sessionStorage.setItem("uid", userId);
+
+        //테스트
+        window.location.href="http://localhost:1227/join";
+    }
+});
+
+$("#public-channelJoin-btn").click((e) => {
+    const channelName = $("#public-channelName").val();
+    const userId = $("#public-nickName").val();
+
+    window.sessionStorage.setItem("channel", channelName);
+    window.sessionStorage.setItem("uid", userId);
+
+    //테스트
     window.location.href="http://localhost:1227/join";
 });
