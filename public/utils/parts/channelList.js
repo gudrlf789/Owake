6fb9367f9548e1b4dd291c62ceb63113636a95a7
park hyeong gil <gudrlf7913@naver.com>
2021-12-30 11:22:17 +0900

    업데이트 기능 코드 수정

--- public/utils/parts/channelList.js
+++ public/utils/parts/channelList.js
@@ -27,20 +27,21 @@ $(document).on("click", ".hidden-data", (e) => {
 });
 
 $(document).on("click", "#channelUpdateBtn", (e) => {
-    const adminId =
-        e.currentTarget.parentNode.parentNode.parentNode.childNodes[0]
-            .children[0].value;
-    const adminPassword =
-        e.currentTarget.parentNode.parentNode.parentNode.childNodes[0]
-            .children[1].value;
-    const channelType = e.currentTarget.children[2].value;
-    const channelName =
-        e.currentTarget.parentNode.parentNode.parentNode.childNodes[0]
-            .children[3].value;
+    const adminId = e.currentTarget.parentNode.parentNode.parentNode.childNodes[0].children[0].value;
+    const adminPassword = e.currentTarget.parentNode.parentNode.parentNode.childNodes[0].children[1].value;
+    const channelType = e.currentTarget.parentNode.parentNode.parentNode.childNodes[0].children[2].value;
+    const channelName = e.currentTarget.parentNode.parentNode.parentNode.childNodes[0].children[3].value;
 
     $("#update_adminId").val(adminId);
     $("#update_adminPassword").val(adminPassword);
     $("#update_channelName").val(channelName);
+    if(channelType === "Private"){
+        $("#update_private").attr("checked", true);
+        $("#update_public").attr("checked", false);
+    }else{
+        $("#update_private").attr("checked", false);
+        $("#update_public").attr("checked", true);
+    }
 
     $("#channelUpdateModal").modal();
 });
--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -2,18 +2,18 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 module.exports = firebaseConfig = {
-    // apiKey: "AIzaSyBpd6nq3grmLc2waySsibG2HNJQEZByRlU",
-    // authDomain: "owakeme-e9aa7.firebaseapp.com",
-    // projectId: "owakeme-e9aa7",
-    // storageBucket: "owakeme-e9aa7.appspot.com",
-    // messagingSenderId: "772053587876",
-    // appId: "1:772053587876:web:721ad4ece17ad853341331",
-    // measurementId: "G-NC7JYR8DT0"
-    apiKey: "AIzaSyDGgY0H8doQ-bdya1F8IeGc21oiy9cXFZE",
+    apiKey: "AIzaSyBpd6nq3grmLc2waySsibG2HNJQEZByRlU",
+    authDomain: "owakeme-e9aa7.firebaseapp.com",
+    projectId: "owakeme-e9aa7",
+    storageBucket: "owakeme-e9aa7.appspot.com",
+    messagingSenderId: "772053587876",
+    appId: "1:772053587876:web:721ad4ece17ad853341331",
+    measurementId: "G-NC7JYR8DT0"
+    /*apiKey: "AIzaSyDGgY0H8doQ-bdya1F8IeGc21oiy9cXFZE",
     authDomain: "owake-db.firebaseapp.com",
     projectId: "owake-db",
     storageBucket: "owake-db.appspot.com",
     messagingSenderId: "633690061634",
     appId: "1:633690061634:web:d4282ce55d049f9247e6db",
-    measurementId: "G-53VRLKQFNC",
+    measurementId: "G-53VRLKQFNC",*/
 };
