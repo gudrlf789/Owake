28f5665af14c42ec6b94fbd0c428d26804d5c949
declan <syas0301@gmail.com>
2021-12-30 16:18:50 +0900

    update

--- public/utils/parts/channelCreate.js
+++ public/utils/parts/channelCreate.js
@@ -1,5 +1,7 @@
 function afterAction(typeFlag) {
-    typeFlag === "private" ? $("#channelPrivateCreate").modal("hide") : $("#channelPublicCreate").modal("hide");
+    typeFlag === "private"
+        ? $("#channelPrivateCreate").modal("hide")
+        : $("#channelPublicCreate").modal("hide");
     $(`#${typeFlag}_adminId`).val("");
     $(`#${typeFlag}_adminPassword`).val("");
     $(`#${typeFlag}_channelName`).val("");
@@ -7,41 +9,59 @@ function afterAction(typeFlag) {
     $(`#${typeFlag}_theme-category`).val("outdoor");
     $(`#${typeFlag}_file_thumnail`).val("");
     $(`#${typeFlag}_channel-description`).val("");
-
 }
 
 function createChannelData(typeFlag) {
     const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
     const imageType = /(.*?)\/(jpg|jpeg|png|gif|bmp)$/;
-    
+
     const formData = new FormData();
 
     formData.append("adminId", $(`#${typeFlag}_adminId`).val());
     formData.append("adminPassword", $(`#${typeFlag}_adminPassword`).val());
-    formData.append("channelType", typeFlag === "private" ? "Private" : "Public");
+    formData.append(
+        "channelType",
+        typeFlag === "private" ? "Private" : "Public"
+    );
     formData.append("channelName", $(`#${typeFlag}_channelName`).val());
-    formData.append("channelPassword",$(`#${typeFlag}_channelPassword`).val());
+    formData.append("channelPassword", $(`#${typeFlag}_channelPassword`).val());
     formData.append("channelCategory", $(`#${typeFlag}_theme-category`).val());
-    formData.append("channelDescription", $(`#${typeFlag}_channel-description`).val());
-    
-    if($(`#${typeFlag}_file_thumnail`)[0].files[0]){
-        if(imageType.test($(`#${typeFlag}_file_thumnail`)[0].files[0].type)){
-            formData.append("image", $(`#${typeFlag}_file_thumnail`)[0].files[0]);
-            formData.append("imageName", $(`#${typeFlag}_file_thumnail`)[0].files[0].name);
-        }else{
+    formData.append(
+        "channelDescription",
+        $(`#${typeFlag}_channel-description`).val()
+    );
+
+    if ($(`#${typeFlag}_file_thumnail`)[0].files[0]) {
+        if (imageType.test($(`#${typeFlag}_file_thumnail`)[0].files[0].type)) {
+            formData.append(
+                "image",
+                $(`#${typeFlag}_file_thumnail`)[0].files[0]
+            );
+            formData.append(
+                "imageName",
+                $(`#${typeFlag}_file_thumnail`)[0].files[0].name
+            );
+        } else {
             alert("You can only select the image file");
             return;
         }
     }
 
-    if (!korean.test(formData.get("adminId")) && !korean.test(formData.get("channelName"))) {
+    if (
+        !korean.test(formData.get("adminId")) &&
+        !korean.test(formData.get("channelName"))
+    ) {
         axios.post("/channel/register", formData).then((res) => {
             if (res.data.success) {
                 alert("The channel has been successfully created");
                 afterAction(typeFlag);
                 callChannelList();
             } else {
-                alert(`ChannelName: ${$(`#${typeFlag}_channelName`).val()} is already existed. please choice another type or channelName`);
+                alert(
+                    `ChannelName: ${$(
+                        `#${typeFlag}_channelName`
+                    ).val()} is already existed. please choice another type or channelName`
+                );
                 $(`#${typeFlag}_channelName`).val("");
             }
         });
