2f7b6ac98a7eae571efaedc1a9d92e36b09116ae
declan <syas0301@gmail.com>
2022-01-04 17:47:08 +0900

    mobilebtn Reszie update

--- public/utils/parts/mobileFixedBtn.js
+++ public/utils/parts/mobileFixedBtn.js
@@ -1,56 +1,60 @@
-$(document).ready((e) => {
-    // Element Create Div
-    const mobileMenu = document.createElement("div");
-    const channelPublicBtn = document.createElement("div");
-    const channelPrivateBtn = document.createElement("div");
-    const channelPublicBtnImg = document.createElement("img");
-    const channelPrivateBtnImg = document.createElement("img");
-    const channelBtnPublicText = document.createElement("p");
-    const channelBtnPrivateText = document.createElement("p");
-    // Element Create End
+// Element Create Div
+const mobileMenu = document.createElement("div");
+const channelPublicBtn = document.createElement("div");
+const channelPrivateBtn = document.createElement("div");
+const channelPublicBtnImg = document.createElement("img");
+const channelPrivateBtnImg = document.createElement("img");
+const channelBtnPublicText = document.createElement("p");
+const channelBtnPrivateText = document.createElement("p");
+// Element Create End
 
-    // ClassName, ID Setting
-    mobileMenu.className = "mobileClickMenu";
-    channelPublicBtn.className = "mobile-channel-btn";
-    channelPrivateBtn.className = "mobile-channel-btn";
-    channelPublicBtnImg.className = "mobile-channel-btn-img";
-    channelPrivateBtnImg.className = "mobile-channel-btn-img";
+// ClassName, ID Setting
+mobileMenu.className = "mobileClickMenu";
+channelPublicBtn.className = "mobile-channel-btn";
+channelPrivateBtn.className = "mobile-channel-btn";
+channelPublicBtnImg.className = "mobile-channel-btn-img";
+channelPrivateBtnImg.className = "mobile-channel-btn-img";
 
-    channelPublicBtn.id = "mobileChannelPublicBtn";
-    channelPrivateBtn.id = "mobileChannelPrivateBtn";
-    // ClassName, ID Setting End
+channelPublicBtn.id = "mobileChannelPublicBtn";
+channelPrivateBtn.id = "mobileChannelPrivateBtn";
+// ClassName, ID Setting End
 
-    mobileMenu.style.padding = "10px";
+mobileMenu.style.padding = "10px";
 
-    channelPublicBtnImg.src = "../../img/channel/public.svg";
-    channelPrivateBtnImg.src = "../../img/channel/private.svg";
+channelPublicBtnImg.src = "../../img/channel/public.svg";
+channelPrivateBtnImg.src = "../../img/channel/private.svg";
 
-    channelBtnPublicText.textContent = "Public";
-    channelBtnPrivateText.textContent = "Private";
+channelBtnPublicText.textContent = "Public";
+channelBtnPrivateText.textContent = "Private";
 
-    channelPublicBtn.append(channelPublicBtnImg, channelBtnPublicText);
-    channelPrivateBtn.append(channelPrivateBtnImg, channelBtnPrivateText);
+channelPublicBtn.append(channelPublicBtnImg, channelBtnPublicText);
+channelPrivateBtn.append(channelPrivateBtnImg, channelBtnPrivateText);
 
-    mobileMenu.append(channelPublicBtn, channelPrivateBtn);
+mobileMenu.append(channelPublicBtn, channelPrivateBtn);
 
-    const bodyContainer = document.querySelector(".body-container");
+const bodyContainer = document.querySelector(".body-container");
 
-    let menuActive = false;
+let menuActive = false;
 
-    $(".mobile-main-fixed-menu").click((e) => {
-        menuActive = !menuActive;
-        if (menuActive == true) {
-            bodyContainer.append(mobileMenu);
-        } else {
-            bodyContainer.removeChild(mobileMenu);
-        }
-    });
+$(".mobile-main-fixed-menu").click((e) => {
+    menuActive = !menuActive;
+    if (menuActive == true) {
+        bodyContainer.append(mobileMenu);
+    } else {
+        bodyContainer.removeChild(mobileMenu);
+    }
+});
 
-    $(document).on("click", "#mobileChannelPublicBtn", (e) => {
-        $("#channelPublicCreate").modal("show");
-    });
+$(document).on("click", "#mobileChannelPublicBtn", (e) => {
+    $("#channelPublicCreate").modal("show");
+});
 
-    $(document).on("click", "#mobileChannelPrivateBtn", (e) => {
-        $("#channelPrivateCreate").modal("show");
-    });
+$(document).on("click", "#mobileChannelPrivateBtn", (e) => {
+    $("#channelPrivateCreate").modal("show");
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        bodyContainer.removeChild(mobileMenu);
+    }
 });
