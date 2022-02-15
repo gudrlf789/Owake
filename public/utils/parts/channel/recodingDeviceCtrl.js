b18272ae9de5dae4b1fd40d938da1e4e258ef5b8
declan <syas0301@gmail.com>
2022-02-15 17:03:24 +0900

    important update

--- public/utils/parts/channel/recodingDeviceCtrl.js
+++ public/utils/parts/channel/recodingDeviceCtrl.js
@@ -30,12 +30,13 @@ export const recodingDeviceCtrl = () => {
     deviceSettingBtn.addEventListener("click", async (e) => {
         $(".cam-list").delegate("a", "click", function (e) {
             videoBox = document.querySelector("#local__videoBox");
-            // if (this.text.includes("back")) {
-            //     switchCamera(this.text);
-            // } else {
-
-            // }
-            switchCamera(this.text);
+            if (this.text.includes("back")) {
+                switchCamera(this.text);
+                videoBox.childNodes[0].childNodes[0].style.transform =
+                    "none !important";
+            } else {
+                switchCamera(this.text);
+            }
         });
         $(".mic-list").delegate("a", "click", function (e) {
             switchMicrophone(this.text);
