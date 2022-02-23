f906d491be9ed09e2e06e1b1d23ace5dd269201b
declan <syas0301@gmail.com>
2022-02-23 15:18:03 +0900

    Fileshare code 원복

--- public/utils/parts/channel/fileShare.js
+++ public/utils/parts/channel/fileShare.js
@@ -8,111 +8,105 @@
 
 export const fileShare = () => {
     // DOM Select
-    const sendBtn = document.querySelector("#sendBtn");
-    const receiveBtn = document.querySelector("#receiveBtn");
-    const keyInput = document.querySelector("#keyInput");
-    const status = document.querySelector("#status");
 
-    const fileInput = document.querySelector("#fileInput");
+    const params = {
+        api_key: "503d6430f3c124e0f239092e9c916b932a869dfe",
+        profile_name: "owake",
+    };
 
-    function updateDevice() {
-        $.ajax({
-            url: "https://send-anywhere.com/web/v1/device",
-            type: "GET",
-            dataType: "jsonp",
-            data: {
-                api_key: "503d6430f3c124e0f239092e9c916b932a869dfe",
-                profile_name: "owake",
-            },
-            cache: false,
-        }).done(function (data) {});
-    }
-    function createKey(files) {
-        let params = { file: [] };
-        let formData = new FormData();
-        for (let i = 0; i < files.length; i++) {
-            let file = files[i];
-            params.file.push({ name: file.name, size: file.size });
-            formData.append("file" + i, file, file.name);
+    $(document).ready(() => {
+        function updateDevice() {
+            $.ajax({
+                url: "https://send-anywhere.com/web/v1/device",
+                type: "GET",
+                dataType: "jsonp",
+                data: params,
+                cache: false,
+            }).done((data) => {});
         }
 
-        $.ajax({
-            url: "https://send-anywhere.com/web/v1/key",
-            type: "GET",
-            data: params,
-            dataType: "jsonp",
-            cache: false,
-        }).done(function (data, textStatus, xhr) {
-            $("#key").text(data.key);
-            sendFile(data.weblink, formData);
-        });
-    }
+        function createKey(files) {
+            let params = { file: [] };
+            let formData = new FormData();
+            for (let i = 0; i < files.length; i++) {
+                let file = files[i];
+                params.file.push({ name: file.name, size: file.size });
+                formData.append("file" + i, file, file.name);
+            }
 
-    function sendFile(url, data) {
-        $.ajax({
-            url: url,
-            type: "POST",
-            processData: false,
-            contentType: false,
-            data: data,
-            cache: false,
-        }).done(function (data) {});
-    }
+            $.ajax({
+                url: "https://send-anywhere.com/web/v1/key",
+                type: "GET",
+                data: params,
+                dataType: "jsonp",
+                cache: false,
+            }).done(function (data, textStatus, xhr) {
+                $("#key").text(data.key);
+                sendFile(data.weblink, formData);
+            });
+        }
 
-    function receiveKey(key) {
-        $("#status").text("waiting");
-        $.ajax({
-            url: "https://send-anywhere.com/web/v1/key/" + key,
-            type: "GET",
-            dataType: "jsonp",
-            timeout: 3000,
-            cache: false,
-        })
-            .done(function (data) {
-                receiveFile(data.weblink);
-                $("#status").text("done");
+        function sendFile(url, data) {
+            $.ajax({
+                url: url,
+                type: "POST",
+                processData: false,
+                contentType: false,
+                data: data,
+                cache: false,
+            }).done(function (data) {});
+        }
+
+        function receiveKey(key) {
+            $("#status").text("waiting");
+            $.ajax({
+                url: "https://send-anywhere.com/web/v1/key/" + key,
+                type: "GET",
+                dataType: "jsonp",
+                timeout: 3000,
+                cache: false,
             })
-            .fail(function (xhr, textStatus, error) {
-                $("#receiveForm .form-group").addClass("has-error");
-                $("#status")
-                    .text("failed")
-                    .removeClass("text-success")
-                    .addClass("text-danger");
-            });
-    }
-
-    function receiveFile(url) {
-        $("<iframe />").attr("src", url).hide().appendTo("body");
-    }
-
-    sendBtn.addEventListener("click", () => {
-        let files = fileInput.files;
-        console.log(files);
-        if (files.length > 0) {
-            createKey(files);
+                .done(function (data) {
+                    receiveFile(data.weblink);
+                    $("#status").text("done");
+                })
+                .fail(function (xhr, textStatus, error) {
+                    $("#receiveForm .form-group").addClass("has-error");
+                    $("#status")
+                        .text("failed")
+                        .removeClass("text-success")
+                        .addClass("text-danger");
+                });
         }
-    });
 
-    receiveBtn.addEventListener("click", () => {
-        receiveKey(keyInput.value);
-    });
-
-    keyInput.addEventListener("keyup", (e) => {
-        if (e.keyCode == 13) {
-            receiveBtn.addEventListener("click", () => {
-                receiveKey(keyInput.value);
-            });
+        function receiveFile(url) {
+            $("<iframe />").attr("src", url).hide().appendTo("body");
         }
-    });
 
-    keyInput.addEventListener("keydown", () => {
-        document
-            .querySelector("#receiveForm .form-group")
-            .classList.remove("has-error");
-        status.text.classList
-            .remove(text - danger)
-            .classList.add("text-success");
-    });
+        $("#sendBtn").click(function () {
+            let files = $("#fileInput").prop("files");
+            if (files.length > 0) {
+                createKey(files);
+            }
+        });
 
-    updateDevice();
+        $("#receiveBtn").click(function () {
+            receiveKey($("#keyInput").val());
+        });
+
+        $("#keyInput").keyup(function (e) {
+            if (e.keyCode == 13) {
+                $("#receiveBtn").click();
+            }
+        });
+
+        $("#keyInput").keydown(function () {
+            $("#receiveForm .form-group").removeClass("has-error");
+            $("#status")
+                .text("")
+                .removeClass("text-danger")
+                .addClass("text-success");
+        });
+        updateDevice();
+    });
 };
