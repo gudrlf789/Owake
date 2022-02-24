702ee83bbf1767c0494fe8a6c2237e7076bc545e
declan <syas0301@gmail.com>
2022-02-24 19:41:54 +0900

    fileShare와 fileDelivery로 나누었음.

--- public/utils/parts/channel/fileShare.js
+++ public/utils/parts/channel/fileShare.js
@@ -1,3 +1,16 @@
+
+/**
+ * @author 전형동
+ * @version 1.0
+ * @data 2022.02.24
+ * @description
+ * FileShare 새로 추가
+ * ---------------- 문제점 ----------------
+ * 1. 소켓 연결시 데이터는 넘어가지만 이미지, 동영상, 텍스트, 오디오 등 제대로 브라우징이 안됨.
+ * 2. 클릭버튼 넘겨야됨.
+ * 3. 호스트 권한과 게스트 권한 넘겨서 클릭버튼 컨트롤 해야됨.
+ */
+
 let fileShareSocket = io();
 let fileShareBtnActive = false;
 let channel = window.sessionStorage.getItem("channel");
@@ -38,18 +51,6 @@ audioEl.preload = "metadata";
 
 fileInputEl.type = "file";
 
-export const fileShare = () => {
-    fileDelivery();
-    fileShareActivate();
-    fileReadAction();
-    fileRemoteSocketAction();
-};
-
-/**
- * @author 전형동
- * @version 1.0 FileShare와 FileTransfer로 함수를 나누었음.
- * @data 2022.02.21
- */
 
 function fileShareActivate() {
     if (fileShareBtn) {
@@ -223,112 +224,8 @@ function fileRemoteSocketAction() {
     });
 }
 
-/**
- * @author 전형동
- * @version 1.0 FileDelivery
- * @data 2022.02.22
- * @description
- * 코드 재조정... Sapari 업데이트 예정
- */
-
-function fileDelivery() {
-    $(document).ready(() => {
-        const params = {
-            api_key: "503d6430f3c124e0f239092e9c916b932a869dfe",
-            profile_name: "owake",
-        };
-        function updateDevice() {
-            $.ajax({
-                url: "https://send-anywhere.com/web/v1/device",
-                type: "GET",
-                dataType: "jsonp",
-                data: params,
-                cache: false,
-            }).done((data) => {});
-        }
-
-        function createKey(files) {
-            let params = { file: [] };
-            let formData = new FormData();
-            for (let i = 0; i < files.length; i++) {
-                let file = files[i];
-                params.file.push({ name: file.name, size: file.size });
-                formData.append("file" + i, file, file.name);
-            }
-
-            $.ajax({
-                url: "https://send-anywhere.com/web/v1/key",
-                type: "GET",
-                data: params,
-                dataType: "jsonp",
-                cache: false,
-            }).done(function (data, textStatus, xhr) {
-                $("#key").text(data.key);
-                sendFile(data.weblink, formData);
-            });
-        }
-
-        function sendFile(url, data) {
-            $.ajax({
-                url: url,
-                type: "POST",
-                processData: false,
-                contentType: false,
-                data: data,
-                cache: false,
-            }).done(function (data) {});
-        }
-
-        function receiveKey(key) {
-            $("#status").text("waiting");
-            $.ajax({
-                url: "https://send-anywhere.com/web/v1/key/" + key,
-                type: "GET",
-                dataType: "jsonp",
-                timeout: 3000,
-                cache: false,
-            })
-                .done(function (data) {
-                    receiveFile(data.weblink);
-                    $("#status").text("done");
-                })
-                .fail(function (xhr, textStatus, error) {
-                    $("#receiveForm .form-group").addClass("has-error");
-                    $("#status")
-                        .text("failed")
-                        .removeClass("text-success")
-                        .addClass("text-danger");
-                });
-        }
-
-        function receiveFile(url) {
-            $("<iframe />").attr("src", url).hide().appendTo("body");
-        }
-
-        $(document).on("click touchstart", "#sendBtn", function (e) {
-            let files = $("#fileInput").prop("files");
-            if (files.length > 0) {
-                createKey(files);
-            }
-        });
-
-        $(document).on("click touchstart", "#receiveBtn", function (e) {
-            receiveKey($("#keyInput").val());
-        });
-
-        $(document).on("keyup", "#keyInput", function (e) {
-            if (e.keyCode == 13) {
-                $("#receiveBtn").click();
-            }
-        });
-
-        $(document).on("keydown", "#keyInput", function (e) {
-            $("#receiveForm .form-group").removeClass("has-error");
-            $("#status")
-                .text("")
-                .removeClass("text-danger")
-                .addClass("text-success");
-        });
-        updateDevice();
-    });
-}
+export const fileShare = () => {
+    fileShareActivate();
+    fileReadAction();
+    fileRemoteSocketAction();
+};
