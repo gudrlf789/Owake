2d3c156af38e028b15ee37d13655fca0adadaceb
declan <syas0301@gmail.com>
2022-02-24 22:21:05 +0900

    fileShare activate

--- public/utils/channel-common.js
+++ public/utils/channel-common.js
@@ -1,6 +1,7 @@
 import { mobileDisplayCtr } from "./parts/channel/chat.js";
 import { f12defense } from "./parts/channel/f12defense.js";
 import { fileDelivery } from "./parts/channel/fileDelivery.js";
+import { fileShare } from "./parts/channel/fileShare.js";
 import { momentShareFunc } from "./parts/channel/momentShare.js";
 import { muteUtilsFunc } from "./parts/channel/muteUtils.js";
 import { optionsBtn } from "./parts/channel/options-btn.js";
@@ -16,6 +17,7 @@ import { browserEvent } from "./parts/channel/browserEvent.js";
 mobileDisplayCtr();
 f12defense();
 fileDelivery();
+fileShare();
 momentShareFunc();
 muteUtilsFunc();
 optionsBtn();
