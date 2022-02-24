f41ddafe9f308e4bc8c51c5b25da55222967a885
declan <syas0301@gmail.com>
2022-02-24 19:17:00 +0900

    socket 새로 추가했던 것들 전부 삭제 :: Agora에서 sdp 인식을 못하는 현상 발생

--- public/utils/rtcClient.js
+++ public/utils/rtcClient.js
@@ -97,7 +97,6 @@ $(document).on("click", ".player", (e) => {
 });
 
 async function join() {
-    // socket.on("connect", handleConnect);
     options.uid = window.sessionStorage.getItem("uid");
     options.channel = window.sessionStorage.getItem("channel");
 
@@ -270,36 +269,4 @@ function handleUserUnpublished(user) {
     delete totalUsers[id];
     delete remoteUsers[id];
     revertLocalTrackToMain(id);
-    // socket.on("disconnect", handleDisconnect);
-}
-
-/**
- * Socket Connect
- * @anthor 전형동
- * @date 2020.02.21
- * @Description Socket Handler Func
- */
-
-function handleConnect() {
-    console.log("Connected to signaling server");
-
-    let myPeerId = socket.id;
-    console.log("My peer id [ " + myPeerId + " ]");
-    joinToChannel();
-}
-
-function handleDisconnect(reason) {
-    console.log("Disconnected from signaling server", { reason: reason });
-}
-
-function joinToChannel() {
-    console.log("join to channel", options.channel);
-    sendToServer("join", {
-        channel: options.channel,
-        peerId: socket.id,
-        peerName: options.uid,
-    });
-}
-async function sendToServer(msg, config = {}) {
-    await socket.emit(msg, config);
 }
