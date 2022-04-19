fa61a1452278412c6840ff0f8af5af28d4bdd831
park hyeong gil <gudrlf7913@naver.com>
2022-04-19 15:10:45 +0900

    이미지, 미디어 파일 구분할수있게 정규표현식 추가

--- public/utils/parts/channel/contents.js
+++ public/utils/parts/channel/contents.js
@@ -1,5 +1,7 @@
 export const contentsFunc = () => {
     let contentsSocket = io();
+    const imageType = /(.*?)\/(jpg|JPG|jpeg|png|gif|bmp)$/i;
+    const medioType = /(.*?)\/(mp4|m4v|avi|wmv|mwa|asf|mpg|mpeg|ts|mkv|mov|3gp|3g2|webm)$/i;
 
     contentsSocket.emit("join-contents", window.sessionStorage.getItem("channel"));
 
@@ -40,6 +42,30 @@ export const contentsFunc = () => {
         });
     });
 
+    $(document).on("click", ".middleContainerBtn", (e) => {
+        const fileType = e.currentTarget.children[0].value;
+        let fileName = e.currentTarget.children[1].value;
+        originUser = e.currentTarget.children[2].innerHTML.split('-')[0];
+        choiceFile = fileName = originUser + "_" + fileName;
+
+        if(imageType.test(fileType)){
+            mainContainer.innerHTML =
+            `
+                <div class="imageFile style="overflow: scroll, width: 100%, height:100%">
+                    <img src="${fileName}" style="width: 100%" />
+                </div>
+            `;
+        }
+        if(medioType.test(fileType)){
+            mainContainer.innerHTML =
+            `
+                <video class="mediaFile" controls style="width: 100%; height: 100%">
+                    <source src="${fileName}">
+                </video>
+            `;
+        }
+    });
+
     contentsSocket.on("input-content", (data) => {
         makeBtnTemplate(data.userName, data.fileType, data.fileName);
     });
@@ -58,6 +84,13 @@ export const contentsFunc = () => {
         }
     });
 
+    contentsSocket.on("volume-remote", (originVolume, playingFile) => {
+        const mediaFile = document.getElementsByClassName("mediaFile");
+        if(mediaFile.length !== 0 && choiceFile === playingFile){
+            mediaFile[0].volume = originVolume;
+        }
+    });
+
     contentsSocket.on("currentTime-remote", (currentTime, playingFile) => {
         const mediaFile = document.getElementsByClassName("mediaFile");
         if(mediaFile.length !== 0 && choiceFile === playingFile){
@@ -65,19 +98,13 @@ export const contentsFunc = () => {
         }
     });
 
-    $(document).on("click", ".middleContainerBtn", (e) => {
-        let fileName = e.currentTarget.children[1].value;
-        originUser = e.currentTarget.children[2].innerHTML.split('-')[0];
-        choiceFile = fileName = originUser + "_" + fileName;
-
-        mainContainer.innerHTML = 
-        `
-            <video class="mediaFile" controls style="width: 100%; height: 100%">
-                <source src="${fileName}">
-            </video>
-        `;
-    });
-
+    /**
+     * @Author 박형길
+     * @param {*} mediaEvent (video, audio)
+     * @Date 2022 04 19
+     * @Description
+     * @returns
+     */
     document.addEventListener('play', function (e) {
         if(originUser === userName) {
             contentsSocket.emit("play-origin", channelName, choiceFile);
@@ -91,7 +118,10 @@ export const contentsFunc = () => {
     }, true);
 
     document.addEventListener('volumechange', function (e) {
-        
+        if(originUser === userName) {
+            const originVolume = document.getElementsByClassName("mediaFile")[0].volume;
+            contentsSocket.emit("volume-origin", channelName, originVolume, choiceFile);
+        }
     }, true);
 
     document.addEventListener('seeked', function (e) {
@@ -101,6 +131,20 @@ export const contentsFunc = () => {
         }
     }, true);
 
+    /**
+     * @Author 박형길
+     * @param {*} mediaEvent (image)
+     * @Date 2022 04 19
+     * @Description
+     * @returns
+     */
+
+     document.addEventListener('scroll', function (e) {
+        if(originUser === userName) {
+            //const currentTime = document.getElementsByClassName("imageFile");
+            //contentsSocket.emit("currentTime-origin", channelName, currentTime, choiceFile);
+        }
+    }, true);
 
     closeModal.addEventListener("click", (e) => {
         contentsSocket.emit("leave-contents", channelName);
