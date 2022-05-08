5499b351fd6a31290dea27fe3c5112a3f6f45100
declan <syas0301@gmail.com>
2022-05-08 20:32:45 +0900

    리모트 유저 접속했을 때 버튼 색상 변경적용, 유저가 접속한 상태라면 분홍색 아니면 흰색

--- public/utils/parts/channel/remoteDisplay.js
+++ public/utils/parts/channel/remoteDisplay.js
@@ -1,3 +1,15 @@
+/**
+ * @author 전형동
+ * @date 2022 05 08
+ * @version 1.0
+ * @description
+ * remoteDisplay 설정
+ *
+ * ---------------- 수정사항 ---------------
+ * 1. 리모트 유저가 접속했을 때 버튼 색상 변경
+ * 2. 유저가 접속한 상태라면 분홍색 아니면 흰색
+ */
+
 const usersBtn = document.querySelector("#users");
 const remotePlayerList = document.querySelector("#remote-playerlist");
 
