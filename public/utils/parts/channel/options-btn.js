c294c61491e3bb6434114c2c5a8226ae412e3f78
declan <syas0301@gmail.com>
2022-05-12 01:09:40 +0900

    screenShare Error 수정

--- public/utils/parts/channel/options-btn.js
+++ public/utils/parts/channel/options-btn.js
@@ -1,10 +1,6 @@
 const navBtn = document.querySelector("#nav__btn");
-const optionsRightContainer = document.querySelector(".options__right");
-const mainLeaveBtn = document.querySelector(".main__leave");
-const optionsImgBtn = document.querySelector(".optionsBtnImg");
 
 let subOptionsActivator = false;
-let btnClickActivator = false;
 
 export const optionsBtn = () => {
     navBtn.addEventListener("click", (e) => {
@@ -12,17 +8,11 @@ export const optionsBtn = () => {
         // subOptionsActivator ? selectOptionsEnable() : selectOptionsDisable();
     });
 
-    if (optionsRightContainer.hidden === false) {
-        leaveOptions();
-    }
+    // if (optionsRightContainer.hidden === false) {
+    //     leaveOptions();
+    // }
 
-    $(document).on("click", ".optionsBtnImg", (e) => {
-        btnClickActivator = !btnClickActivator;
-    });
+    // $(document).on("click", ".optionsBtnImg", (e) => {
+    //     btnClickActivator = !btnClickActivator;
+    // });
 };
-
-function leaveOptions() {
-    mainLeaveBtn.addEventListener("click", () => {
-        window.location.replace("/");
-    });
-}
--- public/utils/parts/channel/responsive.js
+++ public/utils/parts/channel/responsive.js
@@ -7,30 +7,81 @@ export const responsiveFunc = () => {
 
 function playerNameHidden() {
     const playerName = document.querySelector("#local-player-name");
-    const optionsRightContainer = document.querySelector(".options__right");
+    const mainOptions = document.querySelector(".main__options");
+    const subOptions = document.querySelector(".sub__options");
+    const logOutBtn = document.querySelector(".options__right");
+
     const optionsNext = document.querySelector(".nav__right");
     const optionsPrev = document.querySelector(".nav__left");
 
-    const desktopOptions = document.querySelector(".desktop-options");
-    const mobileOptions = document.querySelector(".mobile-options");
-
     const bodyWidth = document.body.offsetWidth;
+
+    let classNameSearch = {};
+    let mainClassNameResult = false;
+    let subClassNameResult = false;
+
     if (bodyWidth < 768) {
         playerName.hidden = true;
-        optionsRightContainer.hidden = true;
-        optionsNext.hidden = true;
-        optionsPrev.hidden = true;
+        optionsNext.hidden = false;
+        optionsPrev.hidden = false;
 
-        desktopOptions.hidden = true;
-        mobileOptions.hidden = false;
+        logOutBtn.hidden = true;
+
+        for (let i = 0; i < mainOptions.classList.length; i++) {
+            classNameSearch.main = mainOptions.classList[i];
+            classNameSearch.sub = subOptions.classList[i];
+
+            if (
+                !classNameSearch.main.includes("swiper-slide") ||
+                !classNameSearch.main.includes("swiper-slide-active") ||
+                !classNameSearch.main.includes("swiper-slide-next")
+            ) {
+                mainOptions.classList.add("swiper-slide");
+                mainOptions.classList.add("swiper-slide-active");
+                mainOptions.classList.add("swiper-slide-next");
+            }
+
+            if (
+                !classNameSearch.sub.includes("swiper-slide") ||
+                !classNameSearch.sub.includes("swiper-slide-active") ||
+                !classNameSearch.sub.includes("swiper-slide-next")
+            ) {
+                subOptions.classList.add("swiper-slide");
+                subOptions.classList.add("swiper-slide-active");
+                subOptions.classList.add("swiper-slide-next");
+            }
+        }
     } else {
         playerName.hidden = false;
-        optionsRightContainer.hidden = false;
         optionsNext.hidden = true;
         optionsPrev.hidden = true;
 
-        desktopOptions.hidden = false;
-        mobileOptions.hidden = true;
+        logOutBtn.hidden = false;
+
+        for (let i = 0; i < mainOptions.classList.length; i++) {
+            classNameSearch.main = mainOptions.classList[i];
+            classNameSearch.sub = subOptions.classList[i];
+
+            if (
+                classNameSearch.main.includes("swiper-slide") ||
+                classNameSearch.main.includes("swiper-slide-active") ||
+                classNameSearch.main.includes("swiper-slide-next")
+            ) {
+                mainOptions.classList.remove("swiper-slide");
+                mainOptions.classList.remove("swiper-slide-active");
+                mainOptions.classList.remove("swiper-slide-next");
+            }
+
+            if (
+                classNameSearch.sub.includes("swiper-slide") ||
+                classNameSearch.sub.includes("swiper-slide-active") ||
+                classNameSearch.sub.includes("swiper-slide-next")
+            ) {
+                subOptions.classList.remove("swiper-slide");
+                subOptions.classList.remove("swiper-slide-active");
+                subOptions.classList.remove("swiper-slide-next");
+            }
+        }
     }
 }
 
