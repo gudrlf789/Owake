a226c6a1b3f368591858236cc1416ab41399b391
declan <syas0301@gmail.com>
2022-06-07 15:12:59 +0900

    map 기능 추가

--- server/server.js
+++ server/server.js
@@ -110,6 +110,11 @@ app.get("/", (req, res, next) => {
     res.render("index");
 });
 
+app.get("/map", (req, res, next) => {
+    // res.render("index");
+    res.render("map");
+});
+
 app.get("/:channelName/:channelType", (req, res) => {
     let appID = "b11ab973693e46b78b73e55aae3a6a11";
 
